7cbfed689ba2c76f65dc636e170dc6357b755559
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-05-31 01:16:29 +0900

    refactor: @Data

--- src/main/java/com/sparta/schedule/dto/comment/UpdateCommentRequest.java
+++ src/main/java/com/sparta/schedule/dto/comment/UpdateCommentRequest.java
@@ -1,9 +1,9 @@
 package com.sparta.schedule.dto.comment;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
+import lombok.Data;
 
-@Getter
+@Data
 public class UpdateCommentRequest {
 
     @NotBlank
